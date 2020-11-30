8f54a796ec0a1cea01d4136282da10530e22c5ff
Pim Everse <pjeverse@gmail.com>
2020-11-30 14:45:47 +0100

    added abstract class card as template

--- src/com/hz/Main.java
+++ src/com/hz/Main.java
@@ -2,27 +2,16 @@ package com.hz;
 
 public class Main {
 
-    // READ THE INSTRUCTIONS IN THIS METHOD, PLEASE!!!
     public static void main(String[] args) {
 
         // Lets print some cards
         Printer printer = new Console();
 
-        // INSTRUCTIONS, READ CAREFULLY !!!!!!!!!!!!!!!!!!!!!!!!!!
-        // For a normal postcard use: true, false
-        // For a graduation card use: false, true
-
-
-        // ONLY SET ONE OF THE BOOLEAN PARAMETERS TO TRUE!!!!
-        // DON'T FORGET THAT!
-
-        // WE NEED TO MAKE THIS MESSY CODE BETTER IMHO, MAYBE ONE OF THE SKILLED
-        // HBO-ICT STUDENTS CAN HELP US OUT.
-
-        // IF WE HAVE TO ADD AN OTHER TYPE OF CARD...
-        // ...I QUIT THIS JOB.
-        PostCard card = new PostCard(printer, false, true);
+        //It is now super easy!
+        PostCard card = new PostCard(printer);
+        GraduationCard card2 = new GraduationCard(printer);
 
         card.print();
+        card2.print();
     }
 }
